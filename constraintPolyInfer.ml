59214ca692a9b45728607a7bb89cacdf21c88544
Arash Afshar <arash.afshar.edu@gmail.com>
2011-09-06 15:28:19 +0430

    add, remove, and deletion are now only allowed in policy section

--- constraintPolyInfer.ml
+++ constraintPolyInfer.ml
@@ -9,7 +9,8 @@ open InferDefs;;
 
 
 exception Undefined_constant of string
-let type_of_const c t setC =
+exception PolicyError of string
+let type_of_const color c t setC =
 let int3 = (tlabeled (tarrow (tlabeled tint nol) (tlabeled (tarrow (tlabeled tint nol) (tlabeled tint nol)) nol)) nol) in
 	let param = (tlabeled (tvar()) nol) in
 	let bool3 = (tlabeled (tarrow param (tlabeled (tarrow param (tlabeled tbool nol)) nol)) nol) in
@@ -24,31 +25,46 @@ let int3 = (tlabeled (tarrow (tlabeled tint nol) (tlabeled (tarrow (tlabeled tin
 			link t  (tlabeled (tarrow (tlabeled tbool nol) (tlabeled (tarrow tAll (tlabeled (tarrow tAll tAll) nol)) nol)) nol); addC t setC in
 		branch
 	| Name ("addLab") -> (* T-RELAB *)
-		let add =
-			let e1			 = evar() in
-			let te1			 = tlabeled (tlab e1) nol in
-			let te2			 = tevar() in
-			let tReturn		 = (tlabeled te2 e1) in
-			link t (tlabeled (tarrow te1 (tlabeled (tarrow te2 tReturn) nol)) nol); (* Should the whole function have the label NOL or not? yes. because in T-APP expects to see a label for the function *)
-			addC t setC in
-		add
+		if (String.compare color "policy") == 0 then
+			begin
+			let add =
+				let e1			 = evar() in
+				let te1			 = tlabeled (tlab e1) nol in
+				let te2			 = tevar() in
+				let tReturn		 = (tlabeled te2 e1) in
+				link t (tlabeled (tarrow te1 (tlabeled (tarrow te2 tReturn) nol)) nol); (* Should the whole function have the label NOL or not? yes. because in T-APP expects to see a label for the function *)
+				addC t setC in
+			add
+			end
+		else
+			raise (PolicyError "In type_of_const: can't add label while color is code!")
 	| Name ("remLab") -> (* T-UNLAB *)
-		let rem =
-			let e2		= evar() in
-			let tPartOf_te1 = tevar() in (*  *)
-			let te1		= tlabeled tPartOf_te1 e2 in
-			let tReturn	= tPartOf_te1 in
-			(*link t (tlabeled (tarrow te1 (tlabeled tPartOf_te1 nol)) nol); addC t setC in*)
-			link t (tlabeled (tarrow te1 tReturn) nol); addC t setC in
-			(* fixme: adding nol to tPartOf_te1 leads to stack_overflow *) (* nol is added to tPartOf_te1 to make sure that it has a label even if e2 was its last label *)
-		rem
+		if (String.compare color "policy") == 0 then
+			begin
+			let rem =
+				let e2		= evar() in
+				let tPartOf_te1 = tevar() in (*  *)
+				let te1		= tlabeled tPartOf_te1 e2 in
+				let tReturn	= tPartOf_te1 in
+				(*link t (tlabeled (tarrow te1 (tlabeled tPartOf_te1 nol)) nol); addC t setC in*)
+				link t (tlabeled (tarrow te1 tReturn) nol); addC t setC in
+				(* fixme: adding nol to tPartOf_te1 leads to stack_overflow *) (* nol is added to tPartOf_te1 to make sure that it has a label even if e2 was its last label *)
+			rem
+			end
+		else
+			raise (PolicyError "In delta: can't remove label while color is code!")
 	| Name ("getLab") -> (* T-GETLAB *)
-		let get =
-			let e2	 = evar() in
-			let te1	 = tlabeled (tvar()) e2 in
-			let tReturn = (tlabeled (tlab e2) nol) in
-			link t (tlabeled (tarrow te1 tReturn) nol); addC t setC in
-		get
+		if (String.compare color "policy") == 0 then
+			begin
+			let get =
+				let e2	 = evar() in
+				let te1	 = tlabeled (tvar()) e2 in
+				let tReturn = (tlabeled (tlab e2) nol) in
+				link t (tlabeled (tarrow te1 tReturn) nol); addC t setC in
+			get
+			end
+		else
+			raise (PolicyError "In delta: can't get label while color is code!")
 	| Lab ("noLab") -> link t (tlabeled (tlab (eexp (Econ noLab))) (evar())); addC t setC
 	| Lab ("high") -> link t (tlabeled (tlab (eexp (Econ high))) (evar())); addC t setC (* T-HIGH *)
 	| Lab ("low") -> link t (tlabeled (tlab (eexp (Econ low))) (evar())); addC t setC (* T-LOW *)
@@ -57,7 +73,7 @@ let int3 = (tlabeled (tarrow (tlabeled tint nol) (tlabeled (tarrow (tlabeled tin
 
 
 exception Free_variable  of var
-let type_of_var tenv x t setC =
+let type_of_var color tenv x t setC =
 	try link t (List.assoc x tenv); addC t setC (* T-existingVAR *)
 	with Not_found -> link t (tevar()); addC t setC;; (* T-newVAR *)
 
@@ -91,39 +107,39 @@ let rec assertEqual labelList label =
 
 (* =================================== poly infer ================================== *)
 
-let rec constraintGen tenv e t setC (*(ETnode expType)*) =
+let rec constraintGen color tenv e t setC (*(ETnode expType)*) =
 	(print_concrete_expr e); print_string(" : "); (print_type t); print_newline();
 	(*expType.etLink <- ETnode (expTypePair e t);*)
 	match e with
-	| Var x -> type_of_var tenv x t setC
-	| Const c -> type_of_const c t setC
+	| Var x -> type_of_var color tenv x t setC
+	| Const c -> type_of_const color c t setC
 	| Fun (x, e) -> (* T-ABS *)
 		let tX = tevar() and tbody = tevar() in
-		let setC1 = constraintGen (extend tenv (x, tX)) e tbody setC  in
+		let setC1 = constraintGen color (extend tenv (x, tX)) e tbody setC  in
 		(* fixme *)
 		let labelList = getAllLabels t in
 		let _ = assertEqual labelList nol in
 		link t (tlabeled (tarrow tX tbody) nol); addC t setC
 	| App (e1, e2) -> (* T-APP *)
 		let te1 = tevar() and te2 = tevar() in
-		let setC1 = constraintGen tenv e1 te1 setC  in
-		let setC2 = constraintGen tenv e2 te2 setC  in
+		let setC1 = constraintGen color tenv e1 te1 setC  in
+		let setC2 = constraintGen color tenv e2 te2 setC  in
 		let labelList = getAllLabels te1 in
 		let _ = assertEqual labelList nol in
 		link te1 (tlabeled (tarrow te2 t) nol); addC te1 setC
 	| Let (x, e1, e2) -> (* T-LET *) (* fixme: In paper, add the case for [let a b c ... = e1 in e2] *) (* also, implement the generalization *)
 		let tfunc = tevar() and te1 = tevar() and te2 = tevar() in
 		let func = create_lam_expr (List.tl x) e1 in
-		let setC1 = constraintGen tenv func te1 setC in
-		let setC2 = constraintGen (extend tenv (List.hd x, tfunc)) e2 te2 setC in
+		let setC1 = constraintGen color tenv func te1 setC in
+		let setC2 = constraintGen color (extend tenv (List.hd x, tfunc)) e2 te2 setC in
 		link tfunc te1; link t te2; addC tfunc setC; addC t setC
 	| LetP (x, e1, e2) ->
 		let tfunc = tevar() and te1 = tevar() and te2 = tevar() in
 		let func = create_lam_expr (List.tl x) e1 in
-		constraintGen tenv func te1 setC;
-		constraintGen (extend tenv (List.hd x, tfunc)) e2 te2 setC;
+		constraintGen "policy" tenv func te1 setC;
+		constraintGen color (extend tenv (List.hd x, tfunc)) e2 te2 setC;
 		link tfunc te1; link t te2; addC tfunc setC; addC t setC
 
-let genConstrSet e t cs (*etPair*) = constraintGen [] e t cs (*etPair*);;
+let genConstrSet e t cs (*etPair*) = constraintGen "code" [] e t cs (*etPair*);;
 
 
